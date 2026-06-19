bd2cfcf9186b50c7d113b0fc24f722b9145a5244
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2026-06-19 16:28:35 +0200

    More robust WS URL retrieval

--- remotestate-demo/src/main.tsx
+++ remotestate-demo/src/main.tsx
@@ -6,10 +6,18 @@ import App from "./App.tsx";
 
 import { RemoteStateProvider } from "remotestate";
 
-const base = (location.host + location.pathname).replace(/\/$/, "");
-const wsUrl = `ws://${base}/ws`;
+function getWsUrl() {
+  const wsUrl = new URLSearchParams(location.search).get("ws");
+  if (wsUrl && wsUrl !== "") {
+    return wsUrl;
+  }
+  const wsBase = (location.host + location.pathname).replace(/\/$/, "");
+  const wsProtocol = location.protocol === "https:" ? "wss:" : "ws:";
+  return `${wsProtocol}//${wsBase}/ws`;
+}
 
-console.info("WS-URL:", wsUrl);
+const wsUrl = getWsUrl();
+console.info("WebSocket URL:", wsUrl);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
