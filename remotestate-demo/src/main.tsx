f853c1b255ecd11659cc7ebc37bc39894eb686f1
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2026-07-08 11:49:27 +0200

    added actions

--- remotestate-demo/src/main.tsx
+++ remotestate-demo/src/main.tsx
@@ -6,7 +6,7 @@ import { type Path, getPathAt, setPathAt } from "remotestate/path";
 
 import "./index.css";
 import App from "./App";
-import type { State } from "./state";
+import type { ItemStatus, State } from "./state";
 
 const wsUrl = getWsUrl();
 console.info("WebSocket URL:", wsUrl);
@@ -14,6 +14,8 @@ console.info("WebSocket URL:", wsUrl);
 const useStore = create<State>()(() => ({ items: [], selected_item_id: null }));
 
 function createFallback() {
+  let last_id = 0;
+
   return createLocalStateClient({
     store: {
       get(path?: Path): unknown {
@@ -27,6 +29,40 @@ function createFallback() {
       dispose() {},
       subscribe(_path: Path, listener: () => void): () => void {
         return useStore.subscribe(listener);
+      },
+    },
+    actions: {
+      add_item(title: string) {
+        useStore.setState((s) => ({
+          items: [
+            ...s.items,
+            {
+              id: ++last_id,
+              created: new Date().toISOString(),
+              title,
+              status: "todo",
+            },
+          ],
+        }));
+      },
+      remove_item(item_id: number) {
+        useStore.setState((s) => ({
+          items: s.items.filter((item) => item.id !== item_id),
+        }));
+      },
+      set_item_status(item_id: number, status: ItemStatus) {
+        useStore.setState((s) => ({
+          items: s.items.map((item) =>
+            item.id === item_id ? { ...item, status } : item,
+          ),
+        }));
+      },
+      set_item_title(item_id: number, title: string) {
+        useStore.setState((s) => ({
+          items: s.items.map((item) =>
+            item.id === item_id ? { ...item, title } : item,
+          ),
+        }));
       },
     },
   });
