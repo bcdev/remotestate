de4e0eb851d1d8229929c0fa831a128af8dd9240
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2026-07-08 07:04:09 +0200

    Started enhanced demo

--- remotestate-demo/src/main.tsx
+++ remotestate-demo/src/main.tsx
@@ -1,28 +1,49 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { create } from "zustand";
+import { RemoteStateProvider, createLocalStateClient } from "remotestate";
+import { type Path, getPathAt, setPathAt } from "remotestate/path";
 
 import "./index.css";
-import App from "./App.tsx";
+import App from "./App";
+import type { State } from "./state";
 
-import { RemoteStateProvider } from "remotestate";
+const wsUrl = getWsUrl();
+console.info("WebSocket URL:", wsUrl);
+
+const useStore = create<State>()(() => ({ items: [], selected_item_id: null }));
+
+function createFallback() {
+  return createLocalStateClient({
+    store: {
+      get(path?: Path): unknown {
+        return getPathAt(useStore.getState(), path);
+      },
+      set(path: Path, value: unknown): void {
+        const newState = setPathAt(useStore.getState(), path, value);
+        useStore.setState(newState);
+      },
+      provide() {},
+      dispose() {},
+      subscribe(_path: Path, listener: () => void): () => void {
+        return useStore.subscribe(listener);
+      },
+    },
+  });
+}
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <RemoteStateProvider url={wsUrl} fallback={createFallback}>
+      <App />
+    </RemoteStateProvider>
+  </StrictMode>,
+);
 
 function getWsUrl() {
   const wsUrl = new URLSearchParams(location.search).get("ws");
   if (wsUrl && wsUrl !== "") {
     return wsUrl;
   }
-  const wsBase = (location.host + location.pathname).replace(/\/$/, "");
-  const wsProtocol = location.protocol === "https:" ? "wss:" : "ws:";
-  return `${wsProtocol}//${wsBase}/ws`;
+  throw new Error("Missing ws query parameter");
 }
-
-const wsUrl = getWsUrl();
-console.info("WebSocket URL:", wsUrl);
-
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <RemoteStateProvider url={wsUrl}>
-      <App />
-    </RemoteStateProvider>
-  </StrictMode>,
-);
